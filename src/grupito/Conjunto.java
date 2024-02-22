3f957d4ace7cf898ba6b0684f4a94888ce118543
micha <micha@Maick>
2024-02-22 09:09:54 +0100

    primer commit

---
+++ src/grupito/Conjunto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package grupito;
+
+/**
+ *
+ * @author micha
+ */
+public class Conjunto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("oliwi");
+    }
+    
+}
